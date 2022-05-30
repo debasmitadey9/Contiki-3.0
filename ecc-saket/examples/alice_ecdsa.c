d772596e46d639fd0395ffd88112cc05049b0562
debasmitadey9 <91532433+debasmitadey9@users.noreply.github.com>
2022-05-30 10:08:29 +0530

    Update alice_ecdsa.c

--- ecc-saket/examples/alice_ecdsa.c
+++ ecc-saket/examples/alice_ecdsa.c
@@ -11,13 +11,7 @@
 #include "powertrace.h"
 #include "energest.h"
 
-#define DEBUG 0
-#if DEBUG
-#include <stdio.h>
-#define PRINTF(...) printf(__VA_ARGS__)
-#else
-#define PRINTF(...) do {} while (0)
-#endif
+
 /*---------------------------------------------------------------------------*/
 PROCESS ( alice_ecdsa_process, "Alice Ecdsa process" );
 PROCESS ( startup_process, "Statup Process" );
@@ -26,9 +20,6 @@ AUTOSTART_PROCESSES ( &startup_process );
 
 point_t pbkey_alice;
 NN_DIGIT prKey_alice[NUMWORDS];
-
-static struct energy_time last;
-static struct energy_time diff;
 
 static void abc_recv ( struct abc_conn* c );
 static const struct abc_callbacks abc_call = {abc_recv};
@@ -54,27 +45,47 @@ static void random_data ( void* ptr, uint16_t len )
 {
         uint16_t i;
 
-        for ( i=0; i<len; i++ ) 
-	{
-                ( ( uint8_t* ) ( ptr ) ) [i] = random_rand() % 100;
-        }
+      //  for ( i=0; i<len; i++ ) 
+	//{
+          //      ( ( uint8_t* ) ( ptr ) ) [i] = random_rand() % 100;
+        //}
+
+	( ( uint8_t* ) ( ptr ) ) [ 0 ]= 0x4d;
+( ( uint8_t* ) ( ptr ) ) [ 1 ]= 0x72 ;
+( ( uint8_t* ) ( ptr ) ) [ 2 ]= 0x45 ;
+( ( uint8_t* ) ( ptr ) ) [ 3 ]= 0xe2 ;
+( ( uint8_t* ) ( ptr ) ) [ 4 ]= 0xd6 ;
+( ( uint8_t* ) ( ptr ) ) [ 5 ]= 0x1c ;
+( ( uint8_t* ) ( ptr ) ) [ 6 ]= 0xfc ;
+( ( uint8_t* ) ( ptr ) ) [ 7 ]= 0xce ;
+( ( uint8_t* ) ( ptr ) ) [ 8 ]= 0x2f ;
+( ( uint8_t* ) ( ptr ) ) [ 9 ]= 0xea ;
+( ( uint8_t* ) ( ptr ) ) [ 10 ]= 0xfd ;
+( ( uint8_t* ) ( ptr ) ) [ 11 ]= 0x7e ;
+( ( uint8_t* ) ( ptr ) ) [ 12 ]= 0x68 ;
+( ( uint8_t* ) ( ptr ) ) [ 13 ]= 0x7c ;
+( ( uint8_t* ) ( ptr ) ) [ 14 ]= 0xdb ;
+( ( uint8_t* ) ( ptr ) ) [ 15 ]= 0x0e ;
 
 }
 
 
 PROCESS_THREAD ( alice_ecdsa_process, ev, data )
 {
+	
 	static struct etimer et;
         PROCESS_EXITHANDLER ( abc_close ( &abc ); )
         PROCESS_BEGIN();
+        int i;
         powertrace_start(CLOCK_SECOND*5);
-        printf("energy cpu: %lu\n", energest_type_time(ENERGEST_TYPE_CPU) );
+        clock_time_t t1 = clock_time();
+  	printf("\nStart Time:%lu \n",t1);
         abc_open ( &abc, 128, &abc_call );
         
-        while ( 1 ) 
-	{
+       // while ( 1 ) 
+	//{
 		
-                etimer_set(&et, CLOCK_SECOND* 2+ random_rand()%(CLOCK_SECOND*2));
+               // etimer_set(&et, CLOCK_SECOND* 6+ random_rand()%(CLOCK_SECOND*6));
   		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
               
                msg_header_t* header;
@@ -88,8 +99,8 @@ PROCESS_THREAD ( alice_ecdsa_process, ev, data )
         
         hton_uint16 ( &header->data_len, MSG_LEN );
 
-	static struct etimer nrg;
-	energest_flush();
+	//static struct etimer nrg;
+	//energest_flush();
        
         ecdsa_sign ( header->msg, MSG_LEN, header->r, header->s, prKey_alice );
         printf("\nSending Signature:%u\n",header->s);
@@ -99,12 +110,15 @@ PROCESS_THREAD ( alice_ecdsa_process, ev, data )
 
         packetbuf_set_datalen ( sizeof ( msg_header_t ) + MSG_LEN );
 	packetbuf_copyfrom(header, 8);
-	printf("\nMsg Sending:%u,%u,%u\n",header->msg[0],header->msg[1],header->msg[2]);
+	for (i=0;i<MSG_LEN;i++)
+	printf("\nMsg Sending:%u\n",header->msg[i]);
         abc_send ( &abc );
-	
+	clock_time_t t2 = clock_time();
+  printf("\nEnd Time:%lu \n",t2);
 	printf("energy cpu: %lu\n", energest_type_time(ENERGEST_TYPE_CPU) );
-        }
-
+       // }
+	//printf("energy cpu: %lu\n", energest_type_time(ENERGEST_TYPE_CPU) );
+	
         PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
@@ -115,21 +129,24 @@ PROCESS_THREAD ( startup_process, ev, data )
         memset ( prKey_alice, 0, NUMWORDS*NN_DIGIT_LEN );
         memset ( pbkey_alice.x, 0, NUMWORDS*NN_DIGIT_LEN );
         memset ( pbkey_alice.y, 0, NUMWORDS*NN_DIGIT_LEN );
-        memset ( pbkey_alice.z, 0, NUMWORDS*NN_DIGIT_LEN );
-        prKey_alice[9] = 0x7b01;
-        prKey_alice[8] = 0x2db7;
-        prKey_alice[7] = 0x681a;
-        prKey_alice[6] = 0x3f28;
-        prKey_alice[5] = 0xb918;
-        prKey_alice[4] = 0x5c8b;
-        prKey_alice[3] = 0x2ac5;
-        prKey_alice[2] = 0xd528;
-        prKey_alice[1] = 0xdecd;
-        prKey_alice[0] = 0x52da;
+       // memset ( pbkey_alice.z, 0, NUMWORDS*NN_DIGIT_LEN );
+
+        prKey_alice[11] = 0xd6d7;
+        prKey_alice[10] = 0xd4d5;
+        prKey_alice[9] = 0xd2d3;
+        prKey_alice[8] = 0xd0d1;
+        prKey_alice[7] = 0xcecf;
+        prKey_alice[6] = 0xcccd;
+        prKey_alice[5] = 0xcacb;
+        prKey_alice[4] = 0xc8c9;
+        prKey_alice[3] = 0xc6c7;
+        prKey_alice[2] = 0xc4c5;
+        prKey_alice[1] = 0xc2c3;
+        prKey_alice[0] = 0xc0c1;
 
         ecc_init();
 	pbkey_alice = gen_pubkey(prKey_alice);
-	printf("Alice Public Key:%u,%u,%u",pbkey_alice.x,pbkey_alice.y,pbkey_alice.z);
+	printf("Alice Public Key:%u,%u",pbkey_alice.x,pbkey_alice.y);
 	ecdsa_init ( &pbkey_alice );
         process_start ( &alice_ecdsa_process, NULL );
         
